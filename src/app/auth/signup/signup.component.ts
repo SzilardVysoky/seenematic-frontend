955cf713eb90dae1b081b5eda2a2dd134687c084
Szilard Vysoky <szilard.vysoky@kosickaakademia.sk>
2025-01-04 17:09:46 +0100

    enhanced validation for registration + placeholders

--- src/app/auth/signup/signup.component.ts
+++ src/app/auth/signup/signup.component.ts
@@ -20,7 +20,48 @@ export class SignupComponent {
 
   constructor(private http: HttpClient, private router: Router, private authService: AuthService) {} // Inject HttpClient, Router, AuthService
 
+  validateInput(): boolean {
+    if (!this.username.trim()) {
+      this.errorMessage = 'Please add a username.';
+      return false;
+    }
+
+    if (this.username.trim().length < 4) {
+      this.errorMessage = 'Username must be at least 4 characters long.';
+      return false;
+    }
+
+    if (!this.email.trim()) {
+      this.errorMessage = 'Please add an email.';
+      return false;
+    }
+
+    const emailRegex = /^[a-zA-Z0-9._%+-]{3,}@[a-zA-Z0-9.-]{2,}\.[a-zA-Z]{2,}$/;
+    if (!emailRegex.test(this.email)) {
+      this.errorMessage = 'Please enter a valid email address.';
+      return false;
+  }
+
+    if (!this.password) {
+      this.errorMessage = 'Password is required. Please fill it.';
+      return false;
+    }
+
+    if (this.password.length < 6) {
+      this.errorMessage = 'Password must be at least 6 characters long.';
+      return false;
+    }
+
+    this.errorMessage = ''; 
+    return true;
+  }
+
   onSignup() {
+
+    if (!this.validateInput()) {
+      return; 
+    }
+
     const url = 'https://seenematic-backend-production.up.railway.app/api/auth/register'; 
     const body = { name: this.username, email: this.email, password: this.password };
 
