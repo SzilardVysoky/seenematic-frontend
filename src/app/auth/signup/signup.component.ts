6baeb76270485add98193a8335a0fe818ab0ae57
Szilard Vysoky <szilard.vysoky@kosickaakademia.sk>
2025-01-07 13:59:56 +0100

    add carousel button styles, add styles to login/signup form, add highlitened red borders to inputfields in case of error

--- src/app/auth/signup/signup.component.ts
+++ src/app/auth/signup/signup.component.ts
@@ -18,6 +18,11 @@ export class SignupComponent {
   email: string = '';
   password: string = '';
   errorMessage: string = '';
+
+  usernameError: boolean = false;
+  emailError: boolean = false;
+  passwordError: boolean = false;
+
   private filter: Filter;
  
 
@@ -35,13 +40,19 @@ export class SignupComponent {
   }
 
   validateInput(): boolean {
+    this.usernameError = false;
+    this.emailError = false;
+    this.passwordError = false;
+
     if (!this.username.trim()) {
       this.errorMessage = 'Please add a username.';
+      this.usernameError = true;
       return false;
     }
 
     if (this.username.trim().length < 3 || this.username.trim().length > 20) {
       this.errorMessage = 'Username must be between 3 and 20 characters long.';
+      this.usernameError = true;
       return false;
     }
 
@@ -50,34 +61,40 @@ export class SignupComponent {
     for (const word of bannedWords) {
       if (normalizedUsername.includes(word)) {
         this.errorMessage = 'Username contains inappropriate content.';
+        this.usernameError = true;
         return false;
       }
     }
 
     if (!this.email.trim()) {
       this.errorMessage = 'Please add an email.';
+      this.emailError = true;
       return false;
     }
 
     const emailRegex = /^[a-zA-Z0-9._%+-]{3,}@[a-zA-Z0-9.-]{2,}\.[a-zA-Z]{2,}$/;
     if (!emailRegex.test(this.email)) {
       this.errorMessage = 'Please enter a valid email address.';
+      this.emailError = true;
       return false;
   }
 
     if (!this.password) {
       this.errorMessage = 'Password is required. Please fill it.';
+      this.passwordError = true;
       return false;
     }
 
     if (this.password.length < 9) {
       this.errorMessage = 'Password must be at least 9 characters long.';
+      this.passwordError = true;
       return false;
     }
 
     const passwordComplexityRegex = /^(?=.*[A-Z])(?=.*\d)/;
     if (!passwordComplexityRegex.test(this.password)) {
       this.errorMessage = 'Password must have at least one uppercase letter and one number.';
+      this.passwordError = true;
       return false;
     }
 
