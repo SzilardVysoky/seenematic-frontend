005fa9736688e5f9f2ed65e0f9835a5463637810
Szilard Vysoky <szilard.vysoky@kosickaakademia.sk>
2025-01-14 10:39:50 +0100

    test2

--- set-env.js
+++ set-env.js
@@ -1,11 +1,26 @@
 const fs = require('fs');
-const targetPath = './src/environments/environment.prod.ts';
 
-const envConfigFile = `
+const devPath = './src/environments/environment.dev.ts';
+const prodPath = './src/environments/environment.prod.ts';
+
+const devConfigFile = `
+export const environment = {
+    production: false,
+    backendUrl: '${process.env.DEV_BACKEND_URL || 'http://localhost:3000'}'
+};
+`;
+
+const prodConfigFile = `
 export const environment = {
     production: true,
     backendUrl: '${process.env.NG_PROD_BACKEND_URL || ''}'
 };
 `;
-fs.writeFileSync(targetPath, envConfigFile);
-console.log(`Environment file generated at ${targetPath}`);
+
+if (!fs.existsSync(devPath)) {
+    fs.writeFileSync(devPath, devConfigFile);
+    console.log(`Development environment file generated at ${devPath}`);
+}
+
+fs.writeFileSync(prodPath, prodConfigFile);
+console.log(`Production environment file generated at ${prodPath}`);
